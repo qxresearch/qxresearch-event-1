66118a445a21af8894c77e9bd5ac7d037399e9d3
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-09-20 00:05:33 +0530

    Update source-code.py

--- Applications/Merge Multiple PDF/source-code.py
+++ Applications/Merge Multiple PDF/source-code.py
@@ -1,1 +1,15 @@
+from PyPDF2 import PdfFileMerger
+import os 
+#var = os.getcwd() For extracting from enother folder
+merger = PdfFileMerger()
+for items in os.listdir():
+  if items.endswith('.pdf')
+    merger.append(items)
+merger.write("Final_pdf.pdf")
+merger = PdfFileMerger()
+fin = file(originalFile, 'rb')
+merger.append(PdfFileReader(fin))
+fin.close()
+os.remove(originalFile)
+merger.close()
 
