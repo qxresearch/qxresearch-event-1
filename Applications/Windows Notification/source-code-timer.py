cc798f6446c833a10ac81f60639f13401c065810
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-09-20 00:06:55 +0530

    Update source-code-timer.py

--- Applications/Windows Notification/source-code-timer.py
+++ Applications/Windows Notification/source-code-timer.py
@@ -1,1 +1,11 @@
-
+from win10toast import ToastNotifier
+import time
+while True:
+  current_time = time.strftime("%H:%M:%S")
+  if current_time=="00.36.00":
+    print(current_time)
+    break
+  else:
+    pass
+hr=ToastNotifier()
+hr.show_toast("alarm","this is the message")
