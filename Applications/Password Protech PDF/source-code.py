3a6734a6103bbe0d704d5f337e8ca250db1e2677
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-09-20 00:04:16 +0530

    Update source-code.py

--- Applications/Password Protech PDF/source-code.py
+++ Applications/Password Protech PDF/source-code.py
@@ -1,1 +1,10 @@
-
+from PyPDF2 import PdfFileWriter, PdfFileReader
+pdfwriter=PdfFileWriter()
+pdf=PdfFileReader("1.pdf")
+for page_num in range(pdf.numPages):
+  pdfwriter.addPage(pdf.getPage(page_num))
+passw="xiaowuc2"
+pdfwriter.encrypt(passw)
+with open('ho.pdf','wb') as f:
+  pdfwriter.write(f)
+  f.close()
