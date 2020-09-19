f57826f0d213247af24fc4434839a0696fd426eb
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-09-20 00:03:27 +0530

    Update source-code.py

--- Applications/Voice Recorder/source-code.py
+++ Applications/Voice Recorder/source-code.py
@@ -1,1 +1,8 @@
-
+import sounddevice
+from scipy.io.wavfile import write
+fs=44100 #sample_rate
+seconds=10
+print("recording...")
+record_voice=sounddevice.rec(int(seconds * fs),samplerate=fs,channels=2)
+sounddevice.wait()
+write("out.wav",fs,record_voice)
