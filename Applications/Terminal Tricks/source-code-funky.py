52a7e26d88d48e1b456630db77c8a73be177f1bd
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-09-20 00:11:14 +0530

    Update source-code-funky.py

--- Applications/Terminal Tricks/source-code-funky.py
+++ Applications/Terminal Tricks/source-code-funky.py
@@ -1,1 +1,16 @@
+import pyfiglet
+word = pyfiglet.figlet_format("qxresearch",font="alphabet")
+print(word)
 
+```
+Output:
+                                       h
+                                       h
+ qqq  x x rrr eee  ss eee  aa rrr  ccc hhh
+q  q   x  r   e e  s  e e a a r   c    h  h
+ qqq  x x r   ee  ss  ee  aaa r    ccc h  h
+   q
+   qq
+
+
+```
