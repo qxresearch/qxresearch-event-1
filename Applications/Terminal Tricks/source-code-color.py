ede70ab102bcff1c0a682989cb231ac90b4165e8
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-09-20 00:10:38 +0530

    Update source-code-color.py

--- Applications/Terminal Tricks/source-code-color.py
+++ Applications/Terminal Tricks/source-code-color.py
@@ -1,1 +1,18 @@
+from colorama import init
+init()
+from colorama import Fore , Back , Style # Fore- font color | Back- Background
 
+print(Fore.GREEN,"hello qxresearcher")
+
+print(Back.RED,"hello qxresearcher")
+
+#to get back to boring B&W: print(Styoe.RESET_ALL)
+
+```
+All Variaton on Colors: 
+
+Fore: BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE RESET
+Back: BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE RESET
+Style: DIM NORMAL BRIGHT RESET_ALL
+  
+```
