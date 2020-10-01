cc6cb3f7e1f02f2c9c20d95e7f17c49b32c15c97
Aritro <aritrochakraborty29@gmail.com>
2020-10-01 17:06:59 +0530

    little bit modified

--- Applications/Voice Recorder/source-code.py
+++ Applications/Voice Recorder/source-code.py
@@ -3,7 +3,7 @@ from scipy.io.wavfile import write
 fs=44100 #sample_rate
 second=int(input("Enter the time duration in second: ")) #enter your required time..
 print("Recording....\n")
-record_voice=sounddevice.rec(int(seconds * fs),samplerate=fs,channels=2)
+record_voice=sounddevice.rec(int(second * fs),samplerate=fs,channels=2)
 sounddevice.wait()
 print("Finished...\ncheck in the given folder.")
 write("Plese provide your reuired folder path..",fs,record_voice) # output will be shown.
