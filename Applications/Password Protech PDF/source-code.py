eee2cbffe33a74448d918e9bb007d461ea89f973
Rachithooda <hoodarachit07@outlook.com>
2020-11-18 18:29:17 +0530

    take real user input for filename

--- Applications/Password Protech PDF/source-code.py
+++ Applications/Password Protech PDF/source-code.py
@@ -1,7 +1,9 @@
+#current password for ho.pdf is 1234
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import getpass
 pdfwriter=PdfFileWriter()
-pdf=PdfFileReader("1.pdf")
+pdf_file = input("Enter Filename: ")
+pdf=PdfFileReader(pdf_file)
 for page_num in range(pdf.numPages):
   pdfwriter.addPage(pdf.getPage(page_num))
 passw=getpass.getpass(prompt='Enter Password: ')
