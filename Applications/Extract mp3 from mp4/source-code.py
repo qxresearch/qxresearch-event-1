b465162afa14ccc4b51e37830ce8552e61367c30
Atul Anand <atulcleaver@gmail.com>
2021-02-04 13:30:01 +0530

    VideoFilePath is not an atribute of moviespy and some comments

--- Applications/Extract mp3 from mp4/source-code.py
+++ Applications/Extract mp3 from mp4/source-code.py
@@ -1,4 +1,14 @@
 import moviepy
-video = moviepy.editor.VideoFilePath("")
-audio=video.audio 
-audio.write_audiofile('1.mp3')
+import moviepy.editor
+
+
+# Get this library installed in your system
+
+# Import your video file
+# Media file should be local
+video = moviepy.editor.VideoFileClip("")    # Put your file path in here
+
+# Convert video to audio
+audio = video.audio
+audio.write_audiofile('new_audio.mp3')
+
