fbbc63cb1bcd052cdab06918cb163d9d680cc0ff
Dhananjay Pai <80339203+dhananjaypai08@users.noreply.github.com>
2021-08-09 00:18:53 +0530

    Add files via upload
    Beep feature which the user can try, made using winsound module

--- Applications/Alarm/alarmtiming.py
+++ Applications/Alarm/alarmtiming.py
@@ -1,12 +1,28 @@
 from datetime import datetime
 from playsound import playsound
+import winsound
+
+
 #input the time in HH:MM where HH is  hour and MM is minutes in 12 hour format
-alarm_date=input('Enter the date on which you want to set the alarm ').strip()
-alarm_time=''.join(input("Enter the time of alarm to be set: HH:MM AM/PM ").split())
+
+
+
+alarm_date=input('Enter the date on which you want to set the alarm: ').strip()
+alarm_time=''.join(input("Enter the time of alarm to be set in HH:MM,AM/PM format: ").split())
+music_or_beep = input("Enter m for a music or b for beep sound: ")
+if music_or_beep=='b':
+    dur=int(input("duration in seconds: "))*1000 #winsound takes in milliseconds
+    freq = int(input("frequency of the noise: ")) #optimal- 500
 alarm_hour=alarm_time[0:2]
 alarm_minute=alarm_time[3:5]
-alarm_period=alarm_time[5:7].upper()
+alarm_period=alarm_time[6:8].upper()
+
+
+
 print('setting alarm.....')
+
+
+
 while True:
     current_time=datetime.now()
     current_hour=current_time.strftime('%I')
@@ -17,5 +33,8 @@ while True:
         print('*'*10)
         print('| '+'Wake up!'+' |')
         print('*'*10)
-        playsound('audio.wav')
+        if music_or_beep=='m':
+            playsound('audio.wav')
+        else:
+            winsound.Beep(freq,dur)
         break
