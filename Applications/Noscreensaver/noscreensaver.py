fd4066a78d960c2a9c3c1bcd8aaebeb3b15d6d52
Sohel Ahmed <69566363+aloner-pro@users.noreply.github.com>
2022-10-02 14:55:19 +0530

    added a application/noscreensaver

---
+++ Applications/Noscreensaver/noscreensaver.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+# A infinite loop
+while 1>0:
+    # makes the mouse sleep or wait for 40 seconds
+    pyautogui.sleep(40)
+    # clicks about in the corner of the screen like
+    # on the co-ordinates of (50,400)
+    pyautogui.click(50,400)
