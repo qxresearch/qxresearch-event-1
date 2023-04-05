6e81385fedae1594fe4f2aad3b9b4656110ca16f
Atanu Paul <atanupaul2411@gmail.com>
2023-04-05 23:05:48 +0530

    code updated

--- Applications/Password Protech PDF/source-code.py
+++ Applications/Password Protech PDF/source-code.py
@@ -1,12 +1,10 @@
 from PyPDF2 import PdfWriter, PdfReader
-pdfwriter = PdfWriter()
-pdf = PdfReader("voter_id.pdf")
+import getpass
+pdfwriter=PdfWriter()
+pdf=PdfReader("1.pdf")
 for page_num in range(len(pdf.pages)):
-    pdfwriter.add_page(pdf.pages[page_num])
-
-password = "14112003"
-pdfwriter.encrypt(password)
-
-with open('protected.pdf', 'wb') as f:
-    pdfwriter.write(f)
-    f.close()
+  pdfwriter.add_page(pdf.pages[page_num])
+passw=getpass.getpass(prompt='Enter Password: ')
+pdfwriter.encrypt(passw)
+with open('ho.pdf','wb') as f:
+  pdfwriter.write(f)
