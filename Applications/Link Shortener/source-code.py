1730a6f2b530741161439b2681d792d3c110147d
WriddhamMandal3 <wriddhammandal1307@gmail.com>
2023-04-10 08:13:32 +0530

    Link Extractor added

--- Applications/Link Shortener/source-code.py
+++
@@ -1,8 +0,0 @@
-import pyshorteners
-
-link = input("enter the link:") #variable
-shortener = pyshorteners.Shortener() #class object
-
-x = shortener.tinyurl.short(link) #shorting the link
-
-print(x)
